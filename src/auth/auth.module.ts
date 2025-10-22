47287ac6d928a9617addf84ab0f60bd2f8a84530
diegoreyerosadaptia <diego@adaptianow.com>
2025-10-22 13:49:12 -0300

    fix: jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,7 +6,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/users/entities/user.entity';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { JwtStrategy } from 'src/utils/strategies/jwt.strategy';
+import { JwtStrategy } from '../utils/strategies/jwt.strategy';
 import { VerificationToken } from './entities/verification-token.entity';
 import { PasswordResetToken } from './entities/password-reset-token.entity';
 
